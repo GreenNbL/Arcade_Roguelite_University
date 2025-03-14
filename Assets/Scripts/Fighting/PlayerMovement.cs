246122ae769a60de3e623a53a9fe6516b8a8ccab
Pavel Malinovskiy <pasha.malinovskiy1095@gmail.com>
2025-03-14 15:29:31 +0300

    added animations for player

--- Assets/Scripts/Fighting/PlayerMovement.cs
+++ Assets/Scripts/Fighting/PlayerMovement.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+
     private Rigidbody2D rb;
+
+    public Animator animator;
 
     void Start()
     {
@@ -13,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         MovePlayer();
+        Attack();
     }
 
     private void MovePlayer()
@@ -20,14 +24,35 @@ public class PlayerMovement : MonoBehaviour
         // Получаем ввод от игрока
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
+        if (moveHorizontal > 0) // Двигаемся вправо
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Лицом вправо
+        }
+        else if (moveHorizontal < 0) // Двигаемся влево
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Лицом влево
+        }
         // Создаем вектор движения
         Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized; // Нормализация для равномерной скорости
 
+        if (movement.magnitude > 0)
+        {
+            animator.SetFloat("speed", 1);
+        }
+        else
+            animator.SetFloat("speed", 0);
         // Перемещаем игрока
         rb.linearVelocity = movement * moveSpeed;
     }
 
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("attack");
+        }
+
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
       
